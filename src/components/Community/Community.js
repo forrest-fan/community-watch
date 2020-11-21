af71251f9799322e02abbe5e38ad1e9fb36af049
Forrest Fan <forrest.c.fan@gmail.com>
2020-11-21 17:58:51 -0500

    added basic community data to component

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -5,9 +5,14 @@ class Community extends Component {
     render() {
         return(
             <div className='Community'>
-                <h1>{this.props.communityData.name}</h1>
-                <h4>{this.props.communityData.municipality.toUpperCase()}</h4>
-
+                <div className='header'>
+                    <h1>{this.props.communityData.name}</h1>
+                    <h4>{this.props.communityData.municipality.toUpperCase()}</h4>
+                </div>
+                <div className='header'>
+                    <h3>${this.props.communityData.avgPrice.toLocaleString()}</h3>
+                    <h3 className={'change ' + (this.props.communityData.change > 0 ? 'gain' : 'loss')}>{(this.props.communityData.change > 0 ? '+' : '') + this.props.communityData.change.toFixed(2)}%</h3>
+                </div>
             </div>
         );
     }
